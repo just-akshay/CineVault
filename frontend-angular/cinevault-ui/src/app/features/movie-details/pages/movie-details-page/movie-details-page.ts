5541d1b80edb57cee84d9e2c0eddc239905b9bb9
akshay <mandemakshay@gmail.com>
2026-06-08 21:02:08 +0530

    Add movie details page using route id

--- frontend-angular/cinevault-ui/src/app/features/movie-details/pages/movie-details-page/movie-details-page.ts
+++ frontend-angular/cinevault-ui/src/app/features/movie-details/pages/movie-details-page/movie-details-page.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { DUMMY_MOVIES } from '../../../../core/constants/dummy-movies';
+import { Movie } from '../../../../core/models/movie.model';
 
 @Component({
   selector: 'app-movie-details-page',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './movie-details-page.html',
   styleUrl: './movie-details-page.scss',
 })
-export class MovieDetailsPage {}
+export class MovieDetailsPage {
+  movie: Movie | undefined;
+
+  constructor(private readonly route: ActivatedRoute) {
+    const movieId = Number(this.route.snapshot.paramMap.get('id'));
+    this.movie = DUMMY_MOVIES.find((movie) => movie.id === movieId);
+  }
+}
